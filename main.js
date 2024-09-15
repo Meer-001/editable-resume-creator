bfe8969aaa602d3d2306c7fa3ee20dae9d183008
Meer-001 <167479939+Meer-001@users.noreply.github.com>
2024-09-15 15:44:50 +0500

    successfully uploaded editable resume.

---
+++ main.js
@@ -0,0 +1,59 @@
+// listing input elements
+// preventdefault is liye hota takay form submit hoty hway page reload na ho
+var _a;
+(_a = document.getElementById('resume')) === null || _a === void 0 ? void 0 : _a.addEventListener('submit', function (event) {
+    event.preventDefault();
+    var nameElement = document.getElementById('name');
+    var fNameElement = document.getElementById('fathername');
+    var emailElement = document.getElementById('email');
+    var contactElement = document.getElementById('number');
+    var addressElement = document.getElementById('address');
+    var educationElement = document.getElementById('education');
+    var experienceElement = document.getElementById('experience');
+    var skillElement = document.getElementById('skill');
+    //creating output
+    if (nameElement && fNameElement && emailElement && contactElement && addressElement && educationElement && experienceElement && skillElement) {
+        var name_1 = nameElement.value;
+        var fName = fNameElement.value;
+        var email = emailElement.value;
+        var contact = contactElement.value;
+        var address = addressElement.value;
+        var education = educationElement.value;
+        var experience = experienceElement.value;
+        var skill = skillElement.value;
+        var generatedOutput = "\n    <h2>My Resume </h2>\n    <p><strong> Name: </strong> <span id='edit-name' class='editable'> ".concat(name_1, "</span></p>\n    <p><strong> Father Name: </strong><span id='edit-father-name' class='editable'> ").concat(fName, "</span></p>\n    <p><strong> Email address: </strong><span id='edit-email-address' class='editable'> ").concat(email, "</span></p>\n    <p><strong> Contact Number: </strong><span id='edit-contact' class='editable'> ").concat(contact, "</span></p>\n    <p><strong> Address: </strong><span id='edit-address' class='editable'> ").concat(address, "</span></p>\n    \n    <h3> Education:</h3> \n    <p id='edit-education' class='editable'>").concat(education, "</p>\n    <h3> Experience:</h3> \n    <p id='edit-experience' class='editable'>").concat(experience, "</p>\n    <h3> Skills:</h3> \n    <p id='edit-skills' class='editable'>").concat(skill, "</p>\n   ");
+        var resumeOutputElement = document.getElementById('generatedOutput');
+        if (resumeOutputElement) {
+            resumeOutputElement.innerHTML = generatedOutput;
+            makeEditable();
+        }
+        else {
+            console.error('Something is mising in element output');
+        }
+        ;
+    }
+});
+function makeEditable() {
+    var elementsEditor = document.querySelectorAll('.editable');
+    elementsEditor.forEach(function (elements) {
+        elements.addEventListener('click', function () {
+            var _a;
+            var currentElement = elements;
+            var currentValue = currentElement.textContent || "";
+            //replace content
+            if (currentElement.tagName === 'p' || currentElement.tagName === 'SPAN') {
+                var input_1 = document.createElement('input');
+                input_1.type = 'text';
+                input_1.value = currentValue;
+                input_1.classList.add('edit-input');
+                input_1.addEventListener('blur', function () {
+                    currentElement.textContent = input_1.value;
+                    currentElement.style.display = 'inline';
+                    input_1.remove();
+                });
+                currentElement.style.display = 'none';
+                (_a = currentElement.parentNode) === null || _a === void 0 ? void 0 : _a.insertBefore(input_1, currentElement);
+            }
+        });
+    });
+}
---
+++ main.ts
@@ -0,0 +1,78 @@
+// listing input elements
+// preventdefault is liye hota takay form submit hoty hway page reload na ho
+
+document.getElementById('resume')?.addEventListener('submit',function(event){
+    event.preventDefault()
+
+    const nameElement = document.getElementById('name') as HTMLInputElement
+    const fNameElement = document.getElementById('fathername') as HTMLInputElement
+    const emailElement = document.getElementById('email') as HTMLInputElement
+    const contactElement = document.getElementById('number') as HTMLInputElement
+    const addressElement = document.getElementById('address') as HTMLInputElement
+    const educationElement = document.getElementById('education') as HTMLInputElement
+    const experienceElement = document.getElementById('experience') as HTMLInputElement
+    const skillElement = document.getElementById('skill') as HTMLInputElement
+
+    //creating output
+
+    if(nameElement && fNameElement && emailElement && contactElement && addressElement && educationElement &&experienceElement &&skillElement){
+        const name= nameElement.value
+        const fName= fNameElement.value
+        const email= emailElement.value
+        const contact= contactElement.value
+        const address= addressElement.value
+        const education= educationElement.value
+        const experience= experienceElement.value
+        const skill= skillElement.value
+    
+
+    const generatedOutput = `
+    <h2>My Resume </h2>
+    <p><strong> Name: </strong> <span id='edit-name' class='editable'> ${name}</span></p>
+    <p><strong> Father Name: </strong><span id='edit-father-name' class='editable'> ${fName}</span></p>
+    <p><strong> Email address: </strong><span id='edit-email-address' class='editable'> ${email}</span></p>
+    <p><strong> Contact Number: </strong><span id='edit-contact' class='editable'> ${contact}</span></p>
+    <p><strong> Address: </strong><span id='edit-address' class='editable'> ${address}</span></p>
+    
+    <h3> Education:</h3> 
+    <p id='edit-education' class='editable'>${education}</p>
+    <h3> Experience:</h3> 
+    <p id='edit-experience' class='editable'>${experience}</p>
+    <h3> Skills:</h3> 
+    <p id='edit-skills' class='editable'>${skill}</p>
+   `;
+   const resumeOutputElement= document.getElementById('generatedOutput')
+   if(resumeOutputElement){
+    resumeOutputElement.innerHTML = generatedOutput
+    makeEditable()
+   }else{
+    console.error('Something is mising in element output')
+   };
+         }
+        });
+        function makeEditable(){
+            const elementsEditor= document.querySelectorAll('.editable');
+            elementsEditor.forEach(elements => {
+                elements.addEventListener('click', function(){
+                    const currentElement =elements as HTMLElement;
+                    const currentValue = currentElement.textContent || "";
+
+                    //replace content
+                    if(currentElement.tagName === 'p' || currentElement.tagName === 'SPAN'){
+                        const input = document.createElement('input')
+                        input.type = 'text'
+                        input.value = currentValue
+                        input.classList.add('edit-input')
+
+                        input.addEventListener('blur', function(){
+                            currentElement.textContent =input.value;
+                            currentElement.style.display ='inline';
+                            input.remove()
+                        })
+
+                        currentElement.style.display = 'none'
+                        currentElement.parentNode?.insertBefore(input, currentElement)
+                    }
+                })
+            })
+        }
